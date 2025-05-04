feb8ae33e45889247f0c02dfdccfc5d023189840
shlomo <shlomka@edu.hac.ac.il>
2025-05-04 16:04:02 +0300

    defrtyj

--- src/HotelBookingForm.cpp
+++ src/HotelBookingForm.cpp
@@ -7,10 +7,10 @@
 HotelBookingForm::HotelBookingForm(sf::RenderWindow& win, DialogueManager* manager)
     : BookingForm(win, manager) 
 { 
-	m_inputFields.push_back(std::make_unique<FieldToInput>(m_yOffset += 50, "Hotel Name:"));
-	m_inputFields.push_back(std::make_unique<FieldToInput>(m_yOffset += 50, "Check-in Date:"));
-	m_inputFields.push_back(std::make_unique<FieldToInput>(m_yOffset += 50, "Check-out Date:"));
-	m_inputFields.push_back(std::make_unique<FieldToInput>(m_yOffset += 50, "Number of Guests:"));
+	m_inputFields.push_back(std::make_unique<InputChar>(m_yOffset += 50, "Hotel Name:"));
+	m_inputFields.push_back(std::make_unique<InputChar>(m_yOffset += 50, "Check-in Date:"));
+	m_inputFields.push_back(std::make_unique<InputChar>(m_yOffset += 50, "Check-out Date:"));
+	m_inputFields.push_back(std::make_unique<InputChar>(m_yOffset += 50, "Number of Guests:"));
 	m_inputFields.push_back(std::make_unique<FieldToInputSelect>(m_yOffset += 50, "Room Type:", std::vector<std::string>{ "Single Room","Double Room","Family Room","Presidential Suite" },0,0));
     setbuttons();
 }
--- src/TrainBookingForm.cpp
+++ src/TrainBookingForm.cpp
@@ -7,10 +7,10 @@
 TrainBookingForm::TrainBookingForm(sf::RenderWindow& win, DialogueManager* manager)
     : BookingForm(win, manager)
 {
-    m_inputFields.push_back(std::make_unique<FieldToInput>(m_yOffset += 50, "Departure Station:"));
-    m_inputFields.push_back(std::make_unique<FieldToInput>(m_yOffset += 50, "Arrival Station:"));
-    m_inputFields.push_back(std::make_unique<FieldToInput>(m_yOffset += 50, "Departure Date:"));
-    m_inputFields.push_back(std::make_unique<FieldToInput>(m_yOffset += 50, "Number of Passengers:"));
+    m_inputFields.push_back(std::make_unique<InputChar>(m_yOffset += 50, "Departure Station:"));
+    m_inputFields.push_back(std::make_unique<InputChar>(m_yOffset += 50, "Arrival Station:"));
+    m_inputFields.push_back(std::make_unique<InputChar>(m_yOffset += 50, "Departure Date:"));
+    m_inputFields.push_back(std::make_unique<InputChar>(m_yOffset += 50, "Number of Passengers:"));
     m_inputFields.push_back(std::make_unique<FieldToInputSelect>(m_yOffset += 50, "Preferred Time:", std::vector<std::string>{"Morning", "Noon", "Evening", "Night", "Don't Care"}, 1,4));
     m_inputFields.push_back(std::make_unique<FieldToInputSelect>(m_yOffset += 50, "Special Requests:", std::vector<std::string>{"Quiet Zone", "Family Section", "First Class", "None"}, 1,3));
     setbuttons();
