7e20b091957ca0e7ec2bf148b7360bcb3d764e25
shimonbr <shimobr@edu.hac.ac.il>
2025-05-04 16:31:25 +0300

    i9kmjnkm

--- src/Email.cpp
+++ src/Email.cpp
@@ -9,54 +9,26 @@ Email::Email(int& yOffset)
 //=========================================
 bool Email::validateInput()
 {
-        if (m_inputString.find(' ') != std::string::npos) {
-            return false;
-        }
+	bool isStrudel = false;
+    for (int i = 0;i < m_input.size();i++)
+    {
+		if (m_input[i] == ' ')
+			return false;
 
-        size_t atPos = m_inputString.find('@');
-        if (atPos == std::string::npos || m_inputString.find('@', atPos + 1) != std::string::npos) {
-            return false;
-        }
+		if (m_input[i] == '@')
+		{
+			if (i == 0 || m_input[i+1] == '.')
+				return false;
+			if (isStrudel)
+				return false;
 
-        std::string username = m_inputString.substr(0, atPos);
-        std::string domainAndSuffix = m_inputString.substr(atPos + 1);
-
-        if (username.empty()) {
-            return false;
-        }
-        for (char c : username) {
-            if (!isValidChar(c)) {
-                return false;
-            }
-        }
-
-        size_t lastDotPos = domainAndSuffix.rfind('.');
-        if (lastDotPos == std::string::npos || lastDotPos == 0 || lastDotPos == domainAndSuffix.length() - 1) {
-            return false;
-        }
-
-        std::string domain = domainAndSuffix.substr(0, lastDotPos);
-        std::string suffix = domainAndSuffix.substr(lastDotPos + 1);
-
-        if (domain.empty()) {
-            return false;
-        }
-        for (char c : domain) {
-            if (!isValidChar(c)) {
-                return false;
-            }
-        }
-
-        if (suffix.length() < 2) {
-            return false;
-        }
-        for (char c : suffix) {
-            if (!std::isalpha(static_cast<unsigned char>(c))) {
-                return false;
-            }
-        }
-
-        return true;
+			isStrudel = true;
+		}
+		if (m_input[i] == '.')
+			if (i + 2 >= m_input.size())
+					return false;
+    }
+	return isStrudel;
 }
 //=========================================
 void Email::drawToPresent(sf::RenderWindow& window, int& yOffset)
@@ -68,9 +40,4 @@ void Email::drawToPresent(sf::RenderWindow& window, int& yOffset)
         m_errorString = "";
 
     Input::drawToPresent(window, yOffset);
-}
-//=========================================
-bool Email::isValidChar(char c) const
-{
-    return std::isalnum(static_cast<unsigned char>(c)) || c == '.' || c == '-' || c == '_';
 }
